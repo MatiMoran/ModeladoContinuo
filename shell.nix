0c2ee198200fbe021180b4b15c0f868c70467772
Matias Moran <matiec52000@hotmail.com>
2026-09-01 00:03:56 -0300

    update

--- shell.nix
+++ shell.nix
@@ -1,21 +1,45 @@
 {pkgs ? import <nixpkgs> {}}:
+
 pkgs.mkShell {
   buildInputs = [
-    pkgs.python312
-    pkgs.python312Packages.virtualenv
+    (pkgs.python314.withPackages (ps: [
+      ps.manim
+      ps.numpy
+      ps.matplotlib
+      ps.pandas
+      ps.scipy
+      ps.sympy
+      ps.seaborn
+      ps.ipython
+      ps.ipykernel
+      ps.jupyter
+      ps.jupyterlab
+      ps.jupyterlab-vim
+      ps.jupyterlab-lsp
+    ]))
     pkgs.poetry
-    pkgs.stdenv
     pkgs.uv
+    pkgs.stdenv
+    pkgs.ffmpeg
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-medium
+        standalone
+        preview
+        dvisvgm;
+    })
   ];
 
   LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
     stdenv.cc.cc.lib
+    cairo
+    pango
+    libGL
+    glib
+    gdal
     zlib
     libpng
     freetype
     fontconfig
-    libGL
-    glib
-    gdal
   ];
 }
